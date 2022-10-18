3a30b5468d2dfee981e3db9e65a89c99002c464f
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-10-18 16:47:30 +0300

    solved the task

--- bakery/urls.py
+++ bakery/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from stores import views as store_views
+
+urlpatterns = [
+    ...
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
